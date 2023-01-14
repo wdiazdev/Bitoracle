d7c1817d5789cfeb7047a83cd8be255d29062de0
Wilfredo Diaz <wdiazdev@gmail.com>
2023-01-13 23:01:56 -0500

    portfolio

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -12,6 +12,8 @@ export const Dashboard = () => {
 
     const [loading, setLoading] = useState(true);
 
+    // const [selected, setSelected] = useState([]);
+
     const fetchCryptoData = () => {
         axios.get(marketDataUrl)
             .then(res => {
@@ -29,14 +31,14 @@ export const Dashboard = () => {
     //* LOADER
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(false)
         setTimeout(() => {
             setLoading(false)
         }, 2000)
     }, []);
 
     const handleFilter = (event) => {
-        const searchCoin = event.target.value
+        const searchCoin = event.target.value.toLocaleLowerCase()
         const newFilter = cryptoData.filter((coin) => {
             return coin.name.toLowerCase().includes(searchCoin) ||
                 coin.symbol.toLowerCase().includes(searchCoin)
@@ -47,8 +49,18 @@ export const Dashboard = () => {
             setFilteredData(newFilter);
     };
 
-    return (
+    const handleSelect = (e) => {
+        e.preventDefault()
+        const selectedCoin = document.getElementById('selected').innerText.toLocaleLowerCase();
+        if (selectedCoin === filteredData.name) {
+            setFilteredData(selectedCoin.name)
+        }
+        setFilteredData([])
+        console.log(filteredData)
+    };
 
+
+    return (
         <>
             {loading
                 ?
@@ -63,10 +75,11 @@ export const Dashboard = () => {
                         <div className='dashboard--data'>
 
                             <div className='search'>
-                                <label>Search for an asset:</label>
+                                <label htmlFor='asset'>Add an Asset:</label>
                                 <input
                                     type='text'
                                     name='asset'
+                                    id='asset'
                                     placeholder='Ex: Bitcoin, Ethereum...'
                                     autoComplete='off'
                                     className='input--search'
@@ -76,33 +89,62 @@ export const Dashboard = () => {
                                     <div className='filtered--container'>
                                         {filteredData.map((coin, index) => {
                                             return (
-                                                <div key={index} className='data--result'>
+                                                <button
+                                                    key={index}
+                                                    className='data--result'
+                                                    onClick={handleSelect}
+                                                >
                                                     <img src={coin.image} alt={coin.name} />
-                                                    <span>{coin.name}</span>
-                                                </div>
+                                                    <span
+                                                        id='selected'
 
+                                                    >
+                                                        {coin.name}
+                                                    </span>
+                                                </button>
                                             )
-
                                         })}
                                     </div>
                                 }
                             </div>
 
 
+                            <div className='portfolio--container'>
+                                <h3>Current Balance:</h3>
+
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Asset</th>
+                                            <th>price</th>
+                                            <th>Quantity</th>
+                                            <th>AVG. Buy Price</th>
+                                            <th>Holdings</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+
+
+
+
+
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+
+                                        </tr>
 
 
 
 
 
 
-
-
-
-
-
-
-                            <div className='assets'>
-                                <h3>Total Portfolio Value:</h3>
+                                    </tbody>
+                                </table>
 
                             </div>
 
