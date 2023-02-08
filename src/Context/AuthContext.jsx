67101ddb2d23a896e42338766191a5ee0217929b
Wilfredo Diaz <wdiazdev@gmail.com>
2023-02-07 22:54:01 -0500

    Firebase move

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -6,7 +6,7 @@ import {
     onAuthStateChanged,
     sendPasswordResetEmail,
 } from 'firebase/auth';
-import { auth } from '../firebase';
+import { auth } from '../Utilities/Firebase';
 
 const UserContext = createContext();
 
