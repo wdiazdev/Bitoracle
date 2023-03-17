cd8127ebadfa57286777be107df7a2fd784fd4b7
Wilfredo Diaz <wdiazdev@gmail.com>
2023-03-16 22:03:02 -0400

    Pagination selected

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -57,8 +57,8 @@ export const Pagination = (
                 onChange={e => setItemsPerPage(e.target.value)}
                 className='pages'
             >
-                <option value='25' >25</option>
-                <option value='50' >50</option>
+                <option value='25'>25</option>
+                <option value='50' selected>50</option>
                 <option value='100'>100</option>
             </select>
 
